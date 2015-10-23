9fe5c05a92c4100b3b4ab42cf2e4004e003e6eaa
Grzegorz Miejski <grzegorz.miejski@allegrogroup.com>
2015-10-24 00:38:03 +0200

    Spring boot project

---
+++ src/test/java/aghacks/music/MusicRecommenderApplicationTests.java
@@ -0,0 +1,16 @@
+package aghacks.music;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MusicRecommenderApplication.class)
+public class MusicRecommenderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
